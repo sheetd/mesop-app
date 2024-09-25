dcfd92e58731b68ce3bc8d209c87519ea8aa5ea9
sheetd <neil@sheetd.com>
2024-09-25 09:25:30 -0400

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+
+import mesop as me
+import mesop.labs as mel
+
+
+@me.page(path="/text_to_text", title="Text I/O Example")
+def app():
+  mel.text_to_text(
+    upper_case_stream,
+    title="Text I/O Example",
+  )
+
+
+def upper_case_stream(s: str):
+  yield s.capitalize()
+  time.sleep(0.5)
+  yield "Done"
+
+# https://google.github.io/mesop/getting-started/installing/
